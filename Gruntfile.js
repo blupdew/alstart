820f6b2a92b12743dd51393f5fef21823878f0ce
Matthieu Bousendorfer <hello@edenpulse.com>
2015-02-10 12:26:49 +0100

    Added Alternative Grunttasks
    added concat & uglify too

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,18 @@ module.exports = function (grunt) {
                 ext: '.min.css'
             }
         },
+        concat: {
+            jsfiles: {
+                src: './js/*.js',
+                dest: './js/prod.js'
+            }
+        },
+        uglify: {
+            jsfiles: {
+                src: './js/prod.js',
+                dest: './js/prod.min.js'
+            }
+        },
         watch: {
             files: ['./css/style.scss'],
             tasks: ['sass', 'cssmin'],
@@ -36,8 +48,29 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //Uncomment to go the LESS way, and remove grunt-sass line
+    /*
     grunt.loadNpmTasks('grunt-contrib-less');
+    */
+
+    //Uncomment to activate concat & uglify jsfiles
+    /*
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    */
+
+    // LESS ALTERNATIVE
+    /*
+    grunt.registerTask('default', ['less', 'cssmin', 'watch']);
+    */
+
+    // Build Sass, concat & uglify Js ALTERNATIVE
+    /*
+    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify', 'watch']);
+    */
 
     grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
 
 };
