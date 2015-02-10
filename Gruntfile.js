cb2f9517fa94c4d30f13ce9352591d1c89ac2253
Matthieu Bousendorfer <hello@edenpulse.com>
2015-02-10 12:00:31 +0100

    Added gruntfile, bower config

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                './css/style.css': './css/style.scss'
+            }
+          }
+        },
+        less: {
+          files: {
+            './css/style.css': './css/style.less'
+          },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: './css/',
+                src: ['style.css'],
+                dest: './css/',
+                ext: '.min.css'
+            }
+        },
+        watch: {
+            files: ['./css/style.scss'],
+            tasks: ['sass', 'cssmin'],
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # alstart
 Choucroute, knacks, et picon bière
+
+## Features :
+- HTML5 Boilerplate .htaccess file
+- Bower
+- Gruntfile for building / optimisations
+- Knacss
+- Basic Schnapsit index.html
+
+## Building :
+- npm install
+- bower update
+- grunt / grunt watch
+
+**If you want to use Less instead of default Sass version, just change:**
+`grunt.registerTask('default', ['sass', 'cssmin', 'watch']);`
+to
+`grunt.registerTask('default', ['less', 'cssmin', 'watch']);`
+in Gruntfile.js
